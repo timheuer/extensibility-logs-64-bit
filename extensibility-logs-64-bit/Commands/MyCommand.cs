63b0e1c77618bbbb877f7d52418a6c86cdb339c5
Tim Heuer <tim@timheuer.com>
2022-02-08 20:31:15 -0800

    Add project files.

---
+++ extensibility-logs-64-bit/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace extensibility_logs_64_bit
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("extensibility_logs_64_bit", "Button clicked");
+        }
+    }
+}
